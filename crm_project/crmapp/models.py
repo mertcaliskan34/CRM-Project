555cebc6db4c9c59f282b595e5b14f367bb77dac
Mert Çalışkan <mert_caliskan34@hotmail.com>
2024-10-01 23:00:36 +0300

    Update models.py

--- crm_project/crmapp/models.py
+++ crm_project/crmapp/models.py
@@ -1,6 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Record(models.Model):
+    
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     
     creation_date = models.DateTimeField(auto_now_add=True)
     
